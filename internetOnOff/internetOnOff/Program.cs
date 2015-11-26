732c6a65dc76741e20258ca6d55df53b38a1ae41
Yong Wang <ywangperl@gmail.com>
2015-11-26 15:21:32 -0800

    add ping to avoid /renew when connected

--- internetOnOff/internetOnOff/Program.cs
+++ internetOnOff/internetOnOff/Program.cs
@@ -7,96 +7,145 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Net;
 using System.Text.RegularExpressions;
+using System.IO;
+using System.Net.NetworkInformation;
+
 
 namespace InternetOnOff
 {
-        class InternetOnOff
+    class InternetOnOff
+    {
+        public static void Main()
         {
-            public static void Main()
+            var url = "http://www.taascloud.com/treeview/data" + "/user/ywangperl@gmail.com.txt";
+            turnInternetOnOff(url);
+
+        }
+
+        public static void turnInternetOnOff(string url)
+        {
+            try
             {
-                turnInternetOnOff();
-            }
-
-            public static void turnInternetOnOff()
-            {
-                var site = "http://www.taascloud.com/treeview/data";
-                var user = "/user/tc.txt";
-
-                var url = site + user;
-
-                try
+                if (pingHost("www.yahoo.com"))
                 {
-                    ipconfig("/renew");
-                    var textFromFile = (new WebClient()).DownloadString(url);
-                    string[] lines = textFromFile.Split('\n');
-                    foreach (string line in lines)
-                    {
-                        RegexOptions options = RegexOptions.IgnoreCase;
-                        Regex r1 = new Regex(@"^\s*Internet\s*connection\s*:\s*\s*startTime\s*=\s*(.+)finishTime\s*=\s*(.+)\s*$", options);
-                        Match match = r1.Match(line);
-                        if (match.Success)
-                        {
-                            var startTime = match.Groups[1].Value;
-                            var finishTime = match.Groups[2].Value;
-                            if (timeIsRight(startTime, finishTime))
-                            {
-                                // ipconfig("/all");
-                                ipconfig("");
-                            }
-                        }
-                        // Console.WriteLine(">" + line);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    ; // Console.WriteLine("turnInternetOnOff is executed");
-                }
-            }
-
-            private static bool timeIsRight(string startTimeStr, string finishTimeStr)
-            {
-
-                DateTime startTime = DateTime.Parse(startTimeStr);
-                DateTime finishTime = DateTime.Parse(finishTimeStr);
-                DateTime currentTime = DateTime.Now;
-
-                // Console.WriteLine(" st {0}  ct {1}  ft{2}", startTime.ToString(), currentTime.ToString(), finishTime.ToString());
-                if ((DateTime.Compare(startTime, currentTime) <= 0) && (DateTime.Compare(currentTime, finishTime) <= 0))
-                {
-                    // Console.WriteLine("Turn On Internet");
-                    // ipconfig("/renew");
+                    ;
                 }
                 else
                 {
-                    // Console.WriteLine("Turn Off Internet");
-                    ipconfig("/release");
+                    ipconfig("/renew");
                 }
-
-                return true;
+                var textFromFile = (new WebClient()).DownloadString(url);
+                string[] lines = textFromFile.Split('\n');
+                foreach (string line in lines)
+                {
+                    RegexOptions options = RegexOptions.IgnoreCase;
+                    Regex r1 = new Regex(@"^\s*Internet\s*blocking\s*:\s*\s*startTime\s*=\s*(.+)finishTime\s*=\s*(.+)\s*$", options);
+                    Match match = r1.Match(line);
+                    if (match.Success)
+                    {
+                        var startTime = match.Groups[1].Value;
+                        var finishTime = match.Groups[2].Value;
+                        Time4BlockingInternet(startTime, finishTime);
+                    }
+                }
             }
-            private static void ipconfig(string args)
+            catch (Exception ex)
             {
-                Process myProcess = new Process();
-                try
-                {
-                    myProcess.StartInfo.UseShellExecute = false;
-                    myProcess.StartInfo.CreateNoWindow = true;
-                    myProcess.StartInfo.RedirectStandardOutput = true;
-                    myProcess.StartInfo.FileName = "c:\\Windows\\System32\\ipconfig.exe";
-                    myProcess.StartInfo.Arguments = args;
-                    myProcess.Start();
-                    string output = myProcess.StandardOutput.ReadToEnd();
-                    // Console.WriteLine(output);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                ; // Console.WriteLine("turnInternetOnOff is executed");
             }
         }
+
+        private static bool pingHost(string nameOrAddress)
+        {
+
+            bool pingable = false;
+            Ping pingSender = new Ping();
+            try
+            {
+                PingReply reply = pingSender.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // 
+            }
+            return pingable;
+        }
+        private static bool Time4BlockingInternet(string startTimeStr, string finishTimeStr)
+        {
+
+            DateTime startTime = DateTime.Parse(startTimeStr);
+            DateTime finishTime = DateTime.Parse(finishTimeStr);
+            DateTime currentTime = DateTime.Now;
+
+            if ((DateTime.Compare(startTime, currentTime) <= 0) && (DateTime.Compare(currentTime, finishTime) <= 0))
+            {
+                // Console.WriteLine("Turn Off Internet");
+                WriteErrorLog("Turn Off Internet");
+                ipconfig("/release");
+
+            }
+            else
+            {
+                WriteErrorLog("Turn On Internet");
+            }
+
+            return true;
+        }
+        private static void ipconfig(string args)
+        {
+            Process myProcess = new Process();
+            try
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.StartInfo.RedirectStandardOutput = true;
+                myProcess.StartInfo.FileName = "c:\\Windows\\System32\\ipconfig.exe";
+                myProcess.StartInfo.Arguments = args;
+                myProcess.Start();
+                string output = myProcess.StandardOutput.ReadToEnd();
+                WriteErrorLog(output);
+            }
+            catch (Exception e)
+            {
+                WriteErrorLog(e.Message);
+            }
+        }
+
+        public static void WriteErrorLog(Exception ex)
+        {
+            System.IO.StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+
+
+    }
 }
 
